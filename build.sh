9f6c895a35416379c5e72f46fcf15882ea70daa3
Optimum <ilushenko04@gmail.com>
2025-04-09 17:33:41 +0300

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 cmake -DCMAKE_BUILD_TYPE=Release -B build
 cmake --build build
-echo "Build complete!"
+if [ -e build/BsuSat_orientation_module.uf2 ]
+then
+    echo "Build complete successfully\!"
+else
+    echo "Build failed\!"
+fi
